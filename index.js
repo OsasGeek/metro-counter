cd18ce5c20580f2f6f4218caf4642b6e5eaac5d0
OsasGeek <99337303+OsasGeek@users.noreply.github.com>
2023-05-27 07:04:37 +0100

    Add metro counter files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
-console.log(saveEl)
 function increment() {
     count += 1
     countEl.textContent = count
